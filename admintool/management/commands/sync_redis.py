2d264b34a751b77289f151392667e918f34e4bf3
Aleksey Romanov <a_romanov@wargaming.net>
2016-02-05 18:20:19 +0300

    Added proper redis connection initializing for sync_redis command

--- admintool/management/commands/sync_redis.py
+++ admintool/management/commands/sync_redis.py
@@ -2,10 +2,12 @@ from django.core.management.base import BaseCommand
 from admintool.models import Players
 import redis
 
+from final.settings import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
+
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for player in Players.objects.all():
-            redis_con = redis.StrictRedis(host='localhost', port=6379, db=0)
+            redis_con = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0, password=REDIS_PASSWORD)
             redis_con.zadd('scores', player.xp, player.id)
